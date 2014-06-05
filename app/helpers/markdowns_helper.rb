52fcd516748d14dbaa838869f180b36c6cd244a8
Yuichi Nagayama <p54321ppp-qe9@yahoo.co.jp>
2014-06-05 12:57:08 +0900

    add options to Redcarpet

--- app/helpers/markdowns_helper.rb
+++ app/helpers/markdowns_helper.rb
@@ -1,6 +1,6 @@
 module MarkdownsHelper
   def markdown(content)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, fenced_code_blocks: true, strikethrough: true)
     markdown.render(content).html_safe
   end
 end
